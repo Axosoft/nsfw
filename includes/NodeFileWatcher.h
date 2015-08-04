50c64ddacced5b742eaf47d9321e243d8e1b0e67
Tyler Wanek <tylerw@axosoft.com>
2015-08-04 13:13:41 -0700

    gitignore build folder, functions for NodeFileWatcher
    setinterval, get interval, removewatch, started implementing callbacks into jsnew

--- includes/NodeFileWatcher.h
+++ includes/NodeFileWatcher.h
@@ -10,14 +10,29 @@ using namespace Nan;
 class NodeFileWatcher : public ObjectWrap {
 public:
   static NAN_MODULE_INIT(Init);
-private:
-  NodeFileWatcher();
-  ~NodeFileWatcher();
-  //set<string> mPaths;
-  static NAN_METHOD(JSNew);
-  static NAN_METHOD(AddWatch);
-  static Persistent<v8::Function> constructor;
 
+private:
+  // Constructors
+  NodeFileWatcher(int pInterval, Callback *cb);
+  NodeFileWatcher(int pInterval, Callback *cb, std::string paths[], int pathsCount);
+  ~NodeFileWatcher();
+
+  // Javascript methods
+  static NAN_METHOD(AddWatch);
+  static NAN_GETTER(GetInterval);
+  static NAN_METHOD(JSNew);
+  static NAN_METHOD(RemoveWatch);
+  static NAN_SETTER(SetInterval);
+
+  // Internal methods
+  void mainLoop();
+
+  // Internal members
+  FW::FileWatcher* mFileWatcher;
+  int mInterval;
+
+  // Nan necessary
+  static Persistent<v8::Function> constructor;
 };
 
 #endif

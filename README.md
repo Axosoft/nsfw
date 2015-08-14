d00169956692eb544024af37c2f85decb33802f5
Tyler Wanek <tylerw@axosoft.com>
2015-08-14 11:06:14 -0700

    readme and additional tests

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # node-sentinel-file-watcher
+<table>
+  <thead>
+    <tr>
+      <th>Linux</th>
+      <th>OS X</th>
+      <th>Windows</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td colspan="2" align="center">
+      </td>
+      <td align="center">
+        <a href="https://ci.appveyor.com/project/implausible/node-simple-file-watcher"><img src="https://ci.appveyor.com/api/projects/status/79ejlq7e60kjmbl6?svg=true"></a>
+      </td>
+    </tr>
+  </tbody>
+</table>
 A simple file watcher library for node.
+
+## Usage
 
 ```js
 var simple = require('node-sentinel-file-watcher');
--- test/test.js
+++ test/test.js
@@ -26,98 +26,172 @@ describe('Node Sentinel File Watcher', function() {
     fse.removeSync(workDir);
   });
 
-  it('can listen for a create event', function() {
-    this.timeout(5000);
-    var createEventFound = false;
-    var newFile = "another_test.file";
-    var inPath = path.resolve(workDir, "test2", "folder2");
+  describe('Basic Functions', function() {
+    it('can listen for a create event', function() {
+      this.timeout(5000);
+      var createEventFound = false;
+      var newFile = "another_test.file";
+      var inPath = path.resolve(workDir, "test2", "folder2");
 
-    function findCreateEvent(element, index, array) {
-      if (element.action === "CREATED"
-        && element.directory === path.resolve(inPath)
-        && element.file === newFile)
-      {
-          createEventFound = true;
+      function findCreateEvent(element, index, array) {
+        if (element.action === "CREATED"
+          && element.directory === path.resolve(inPath)
+          && element.file === newFile)
+        {
+            createEventFound = true;
+        }
       }
-    }
-    var watch = new nsfw("./mockfs", function(events) {
-      events.forEach(findCreateEvent);
-    });
-    watch.start();
-
-    return Promise
-      .delay(1000)
-      .then(function() {
-        var fd = fse.openSync(path.join(inPath, newFile), "w");
-        fse.writeSync(fd, "Peanuts, on occasion, rain from the skies.");
-        fse.closeSync(fd);
-      })
-      .delay(3000)
-      .then(function() {
-        assert.equal(createEventFound, true, "NSFW did not hear the create event.");
+      var watch = new nsfw("./mockfs", function(events) {
+        events.forEach(findCreateEvent);
       });
+      watch.start();
+
+      return Promise
+        .delay(1000)
+        .then(function() {
+          var fd = fse.openSync(path.join(inPath, newFile), "w");
+          fse.writeSync(fd, "Peanuts, on occasion, rain from the skies.");
+          fse.closeSync(fd);
+        })
+        .delay(2000)
+        .then(function() {
+          assert.equal(createEventFound, true, "NSFW did not hear the create event.");
+        });
+    });
+
+    it('can listen for a delete event', function() {
+      this.timeout(5000);
+      var deleteEventFound = false;
+      var file = "testing3.file";
+      var inPath = path.resolve(workDir, "test3");
+
+      function findDeleteEvent(element, index, array) {
+        if (element.action === "DELETED"
+          && element.directory === path.resolve(inPath)
+          && element.file === file)
+        {
+            deleteEventFound = true;
+        }
+      }
+      var watch = new nsfw("./mockfs", function(events) {
+        events.forEach(findDeleteEvent);
+      });
+      watch.start();
+
+      return Promise
+        .delay(1000)
+        .then(function() {
+          fse.remove(path.join(inPath, file));
+        })
+        .delay(2000)
+        .then(function() {
+          assert.equal(deleteEventFound, true, "NSFW did not hear the delete event.");
+        });
+    });
+
+
+    it('can listen for a change event', function() {
+      this.timeout(5000);
+      var changeEventFound = false;
+      var file = "testing0.file";
+      var inPath = path.resolve(workDir, "test0");
+
+      function findDeleteEvent(element, index, array) {
+        if (element.action === "CHANGED"
+          && element.directory === path.resolve(inPath)
+          && element.file === file)
+        {
+            changeEventFound = true;
+        }
+      }
+      var watch = new nsfw("./mockfs", function(events) {
+        events.forEach(findDeleteEvent);
+      });
+      watch.start();
+
+      return Promise
+        .delay(1000)
+        .then(function() {
+          var fd = fse.openSync(path.join(inPath, file), "w");
+          fse.writeSync(fd, "At times, sunflower seeds are all that is life.");
+          fse.closeSync(fd);
+        })
+        .delay(2000)
+        .then(function() {
+          assert.equal(changeEventFound, true, "NSFW did not hear the change event.");
+        });
+    });
   });
+  describe("Recursive Functions", function() {
+    it('can listen for the creation of directory and its subtree', function() {
+      this.timeout(5000);
+      var inPath = path.resolve(workDir, "test1");
+      var outPath = path.resolve(workDir, "test6");
+      var createdCount = 0;
 
-  it('can listen for a delete event', function() {
-    this.timeout(5000);
-    var deleteEventFound = false;
-    var file = "testing3.file";
-    var inPath = path.resolve(workDir, "test3");
-
-    function findDeleteEvent(element, index, array) {
-      if (element.action === "DELETED"
-        && element.directory === path.resolve(inPath)
-        && element.file === file)
-      {
-          deleteEventFound = true;
+      function findCreateEvent(element, index, array) {
+        if (element.action === "CREATED")
+        {
+          if (element.directory === outPath
+              && (element.file === "testing1.file" || element.file === "folder1"))
+          {
+            createdCount++;
+          }
+          else if (element.directory === workDir && element.file === "test6")
+          {
+            createdCount++;
+          }
+        }
       }
-    }
-    var watch = new nsfw("./mockfs", function(events) {
-      events.forEach(findDeleteEvent);
-    });
-    watch.start();
-
-    return Promise
-      .delay(1000)
-      .then(function() {
-        fse.remove(path.join(inPath, file));
-      })
-      .delay(3000)
-      .then(function() {
-        assert.equal(deleteEventFound, true, "NSFW did not hear the delete event.");
+      var watch = new nsfw("./mockfs", function(events) {
+        events.forEach(findCreateEvent);
       });
-  });
+      watch.start();
 
+      return Promise
+        .delay(1000)
+        .then(function() {
+          fse.copySync(inPath, outPath);
+        })
+        .delay(2000)
+        .then(function() {
+          assert.equal(createdCount, 3, "NSFW did not hear all 3 delete events.");
+        });
+    });
 
-  it('can listen for a change event', function() {
-    this.timeout(5000);
-    var changeEventFound = false;
-    var file = "testing0.file";
-    var inPath = path.resolve(workDir, "test0");
+    it('can listen for the destruction of directory and its subtree', function() {
+      this.timeout(5000);
+      var inPath = path.resolve(workDir, "test4");
+      var deletionCount = 0;
 
-    function findDeleteEvent(element, index, array) {
-      if (element.action === "CHANGED"
-        && element.directory === path.resolve(inPath)
-        && element.file === file)
-      {
-          changeEventFound = true;
+      function findDeleteEvent(element, index, array) {
+        if (element.action === "DELETED")
+        {
+          if (element.directory === path.resolve(inPath)
+              && (element.file === "testing4.file" || element.file === "folder4"))
+          {
+            deletionCount++;
+          }
+          else if (element.directory === workDir && element.file === "test4")
+          {
+            deletionCount++;
+          }
+        }
       }
-    }
-    var watch = new nsfw("./mockfs", function(events) {
-      events.forEach(findDeleteEvent);
-    });
-    watch.start();
-
-    return Promise
-      .delay(1000)
-      .then(function() {
-        var fd = fse.openSync(path.join(inPath, file), "w");
-        fse.writeSync(fd, "At times, sunflower seeds are all that is life.");
-        fse.closeSync(fd);
-      })
-      .delay(3000)
-      .then(function() {
-        assert.equal(changeEventFound, true, "NSFW did not hear the change event.");
+      var watch = new nsfw("./mockfs", function(events) {
+        events.forEach(findDeleteEvent);
       });
+      watch.start();
+
+      return Promise
+        .delay(1000)
+        .then(function() {
+          fse.remove(inPath);
+        })
+        .delay(2000)
+        .then(function() {
+          assert.equal(deletionCount, 3, "NSFW did not hear all 3 delete events.");
+        });
+    });
   });
 });

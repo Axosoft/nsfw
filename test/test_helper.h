d9bb3c9ee349ae50055bc186478e0f7d7abae7bc
Patrik Fiedler <patrik.fiedler@nexenio.com>
2017-09-25 16:18:31 +0200

    modify cmake and add ctest

--- test/test_helper.h
+++ test/test_helper.h
@@ -1,42 +1,6 @@
-#if defined(_WIN32)
-#include <direct.h>
-#elif defined(__linux__)
-#include <sys/types.h>
-#include <sys/stat.h>
-#endif
-
 #include <iostream>
 #include <fstream>
 #include <cstdio>
-
-void makeDirectory(const std::string &path)
-{
-#if defined(_WIN32)
-    _mkdir(path.c_str());
-#elif defined(__linux__)
-    mkdir(path.c_str(), 0777);
-#endif
-}
-
-void makeFile(const std::string &path)
-{
-    std::ofstream(path.c_str());
-}
-
-void renameFile(const std::string &oldPath, const std::string &newPath)
-{
-    std::rename(oldPath.c_str(), newPath.c_str());
-}
-
-void modifyFile(const std::string &path)
-{
-    std::ofstream(path.c_str()).put('a');
-}
-
-void removeFile(const std::string &path)
-{
-    std::remove(path.c_str());
-}
 
 class DummyFile
 {

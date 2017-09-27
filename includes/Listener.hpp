07fe8721437da9e6a02a20c69cff7ce9b9031ffc
Patrik Fiedler <patrik.fiedler@nexenio.com>
2017-09-27 13:34:49 +0200

    review issues

--- includes/Listener.hpp
+++ includes/Listener.hpp
@@ -15,11 +15,11 @@ struct is_instantiation<Template, Template<Ts...>> : std::true_type {};
 template <class CallbackType>
 class Listener
 {
-    static_assert(is_instantiation<std::function, CallbackType>::value,
+  static_assert(is_instantiation<std::function, CallbackType>::value,
                   "CallbackType must be an instance of std::function");
 
 public:
-    using CallbackHandle = int;
+  using CallbackHandle = int;
 
 private:
   std::map<CallbackHandle, CallbackType> mListeners;
@@ -27,31 +27,31 @@ private:
   int mHandleCount{0};
 
 public:
-    CallbackHandle registerCallback(const CallbackType callback)
-    {
-        std::lock_guard<std::mutex> lock(mListenersMutex);
-        mListeners[++mHandleCount] = callback;
-        return mHandleCount;
-    }
+  CallbackHandle registerCallback(const CallbackType callback)
+  {
+    std::lock_guard<std::mutex> lock(mListenersMutex);
+    mListeners[++mHandleCount] = callback;
+    return mHandleCount;
+  }
 
-    void deregisterCallback(const CallbackHandle &id)
-    {
-        std::lock_guard<std::mutex> lock(mListenersMutex);
-        auto it = mListeners.find(id);
+  void deregisterCallback(const CallbackHandle &id)
+  {
+    std::lock_guard<std::mutex> lock(mListenersMutex);
+    auto it = mListeners.find(id);
 
-        if (it != mListeners.end()) {
-            mListeners.erase(it);
-        }
+    if (it != mListeners.end()) {
+       mListeners.erase(it);
     }
+  }
 
-  protected:
-    template<typename... Args>
-    void notify(Args&&... args) {
-        std::lock_guard<std::mutex> lock(mListenersMutex);
-        for(const auto &func : mListeners) {
-            func.second(std::move(std::forward<Args>(args)...));
-        }
+protected:
+  template<typename... Args>
+  void notify(Args&&... args) {
+    std::lock_guard<std::mutex> lock(mListenersMutex);
+    for(const auto &func : mListeners) {
+      func.second(std::move(std::forward<Args>(args))...);
     }
+  }
 };
 
 #endif
--- src/transforms/ExcludeDirectories.cpp
+++ src/transforms/ExcludeDirectories.cpp
@@ -5,14 +5,14 @@ ExcludeDirectories::ExcludeDirectories(const std::string &regex) : mRegex(regex)
 }
 
 VecEvents ExcludeDirectories::transform(VecEvents vecEvents) {
-    std::regex self_regex(mRegex,
-                          std::regex_constants::ECMAScript
+  std::regex self_regex(mRegex,
+                        std::regex_constants::ECMAScript
                         | std::regex_constants::icase);
-    vecEvents->erase(std::remove_if(vecEvents->begin(),
-                                    vecEvents->end(),
-                                    [this, &self_regex](std::unique_ptr<Event> &event){
-      return std::regex_search(event->directory, self_regex);
-    }), vecEvents->end());
+  vecEvents->erase(std::remove_if(vecEvents->begin(),
+                                  vecEvents->end(),
+                                  [this, &self_regex](std::unique_ptr<Event> &event){
+    return std::regex_search(event->directory, self_regex);
+  }), vecEvents->end());
 
-    return vecEvents;
+  return vecEvents;
 }

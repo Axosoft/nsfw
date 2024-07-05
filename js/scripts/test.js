f845ced13e7b8cdc0212dd328209f0af8cde03dd
Julian Mesa <julian.mesa@gitkraken.com>
2024-07-05 09:34:23 +0200

    Fix run mocha tests

--- js/scripts/test.js
+++ js/scripts/test.js
@@ -3,10 +3,10 @@ const cp = require('child_process');
 if (process.platform !== 'win32' && process.platform !== 'darwin') {
   // When ran as a npm script we can invoke npm bins such as node-gyp and mocha directly
   exec('node-gyp rebuild', { env: { ...process.env, NSFW_TEST_SLOW: 1 } });
-  exec('mocha --exit --expose-gc js/spec/index-slow-spec.js');
+  exec('mocha --exit --v8-expose-gc js/spec/index-slow-spec.js');
   exec('node-gyp rebuild');
 }
-exec('mocha --exit --expose-gc js/spec/index-spec.js');
+exec('mocha --exit --v8-expose-gc js/spec/index-spec.js');
 
 /**
  * @param {string} commandline ...
